6d983b5e722d650c8e98e01c0b0ed3deb2520066
Victor <victor18241@gmail.com>
2024-07-12 17:16:55 +0200

    First commit

---
+++ part1-react/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+const Description = () => {
+  return <p>Esta es la app del curso fullsyack bootcamp</p>
+}
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <Mensaje color="red" message='Estamos trabajando' />
+      <Mensaje color="green" message='En un curso'/>
+      <Mensaje color="yellow" message='de React'/>
+      <Description />
+    </div>
+  );
+}
+
+export default App;
