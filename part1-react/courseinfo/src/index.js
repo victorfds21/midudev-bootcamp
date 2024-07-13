d3e86228d8c9e064aeb19bdf9730a56c3885034d
Victor <victor18241@gmail.com>
2024-07-13 11:10:53 +0200

    change name const

--- part1-react/courseinfo/src/index.js
+++ part1-react/courseinfo/src/index.js
@@ -2,29 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 /** Funciona igualmente y es mas corta
- const Title = ({course}) => <h1>{course}</h1>
+ const Header = ({course}) => <h1>{course}</h1>
  */
-const Title = (props) =>{
+const Header = (props) =>{
   return <h1>{props.course}</h1>
 }
 
 // Funciona igualmente y es mas corta
- const Parraf = ({part,exercises}) => <p> {part} {exercises} </p>
+ const Content = ({part,exercises}) => <p> {part} {exercises} </p>
  
-// const Parraf = (props) => {
+// const Content = (props) => {
 // return <p> {props.part} {props.exercises} </p>
 // }
+
+const Total = (props) => {
+ return <p>{props.text} {props.total}</p>
+}
 
 
 const App = () => {
   
   return (
     <div>
-      < Title course='Half Stack application development'/>
-      < Parraf part = 'Fundamentals of React' exercises = '10'/>
-      < Parraf part = 'Using props to pass data' exercises = '7'/>
-      < Parraf part = 'State of a component' exercises = '14'/>
-      < Parraf part = 'Number of exercises ' exercises = {10+7+14}/>
+      < Header course='Half Stack application development'/>
+      < Content part = 'Fundamentals of React' exercises = '10'/>
+      < Content part = 'Using props to pass data' exercises = '7'/>
+      < Content part = 'State of a component' exercises = '14'/>
+      < Total text = 'Number of exercises ' total = {10+7+14}/>
      
     </div>
   )
